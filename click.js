c06c0b740f05abee6d30a15f0ef1bb9d96149e6a
Malvin-mi <michaelogunyemi3@gmail.com>
2021-11-03 11:47:30 +0100

    made changes for mobile screens

--- click.js
+++ click.js
@@ -8,7 +8,7 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${clickPosts}`)
     let postLayout = document.getElementById('post-layout');
     html = `
     <div class=""> 
-               <div class="card h-100 w-50">
+               <div class="card h-100 w-100">
                   <div class="card-body">
                     <div class= "d-flex justify-content-end">
                     <h5 class= "num text-info">${data.id}</h5>
